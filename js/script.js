cd675f6cd6b489a377240fd482feb6b2473c81f5
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-04-22 17:29:00 +0000

    almost done if  else

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 domeic dangelo All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+const inputOption1 = Math.floor(Math.random() * 6) + 1
+const inputOption2 = Math.floor(Math.random() * -6) + 1
+
+/**
+ * This function checks if the user has selected the random number from above.
+ */
+// eslint-disable-next-line no-unused-vars
+function calculate() {
+  // input
+  const userInput = parseInt(document.getElementById("kind-of-number").checked)
+
+  // process
+  if(userInput === 1) {
+    //output
+    document.getElementById('result').innerHTML =
+      '<p>The random number is:' + inputOption1 + '</p>'
+  }
+  else (userInput === 2); {
+    //output
+    document.getElementById('result').innerHTML =
+      '<p>The random number is:' + inputOption2 + '</p>'
+  }
+}
